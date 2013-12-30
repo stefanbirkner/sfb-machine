31a3874c1bed5f61e813a3ee35501db323fdbb1f
Stefan Birkner <mail@stefan-birkner.de>
2013-12-31 00:24:17 +0100

    Add installation script for arbitrary software.

---
+++ install_non_dev_software.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Install software without development context.
+
+echo "deb http://archive.canonical.com/ubuntu/ saucy partner" > /etc/apt/sources.list.d/canonical_partner.list
+
+apt-get -y update
+apt-get -y install keychain
+apt-get -y install nautilus-dropbox
+apt-get -y install keepass2
