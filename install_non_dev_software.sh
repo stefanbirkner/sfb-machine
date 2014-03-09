95b961cdbec8f760a536d27b87f3a3db1a74eb75
Stefan Birkner <mail@stefan-birkner.de>
2014-03-09 17:44:56 +0100

    Add calibre.

--- install_non_dev_software.sh
+++ install_non_dev_software.sh
@@ -4,6 +4,7 @@
 echo "deb http://archive.canonical.com/ubuntu/ saucy partner" > /etc/apt/sources.list.d/canonical_partner.list
 
 apt-get -y update
+apt-get -y install calibre
 apt-get -y install keychain
 apt-get -y install nautilus-dropbox
 apt-get -y install keepass2
