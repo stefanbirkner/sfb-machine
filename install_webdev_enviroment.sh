8a7d1890b85a0fc08c64d8728cbd3f2de24919d6
Stefan Birkner <mail@stefan-birkner.de>
2014-05-24 14:40:15 +0200

    Add Yeoman.

--- install_webdev_enviroment.sh
+++ install_webdev_enviroment.sh
@@ -4,8 +4,9 @@
 apt-get -y update
 apt-get -y install npm
 ln -s /usr/bin/nodejs /usr/bin/node
+npm install -g bower
+npm install -g yo
 npm install -g grunt-cli
-npm install -g bower 
 
 #Use gem to install jekyll, because the debian package is outdated.
 apt-get install ruby1.9.1-dev
