398a17c32f96e1814dcc0177825821314c276ed1
Stefan Birkner <mail@stefan-birkner.de>
2014-05-24 14:40:52 +0200

    Add Vagrant.

--- install_dev_basics.sh
+++ install_dev_basics.sh
@@ -4,5 +4,7 @@
 apt-get -y update
 apt-get -y install meld
 apt-get -y install tree
+apt-get -y install vagrant
 apt-get -y install vim
+apt-get -y install virtualbox
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
