ff91cca12983e818c6b88320fca5c2597780b465
Stefan Birkner <mail@stefan-birkner.de>
2014-05-30 09:11:54 +0200

    Add Latex.

--- install_non_dev_software.sh
+++ install_non_dev_software.sh
@@ -8,4 +8,8 @@ apt-get -y install calibre
 apt-get -y install keychain
 apt-get -y install nautilus-dropbox
 apt-get -y install keepass2
-apt-get -y install steam 
+apt-get -y install steam
+apt-get -y texlive-fonts-recommended
+apt-get -y texlive-lang-german
+apt-get -y texlive-latex-base
+apt-get -y texlive-latex-recommended
