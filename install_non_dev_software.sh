2ba87073cb649c1e57edca2bc15fb315dc8c0977
Stefan Birkner <github@stefan-birkner.de>
2015-04-30 20:28:36 +0200

    Fix install_non_dev_software.sh
    Some apt-get command were missing the install instruction.

--- install_non_dev_software.sh
+++ install_non_dev_software.sh
@@ -9,7 +9,7 @@ apt-get -y install keychain
 apt-get -y install nautilus-dropbox
 apt-get -y install keepass2
 apt-get -y install steam
-apt-get -y texlive-fonts-recommended
-apt-get -y texlive-lang-german
-apt-get -y texlive-latex-base
-apt-get -y texlive-latex-recommended
+apt-get -y install texlive-fonts-recommended
+apt-get -y install texlive-lang-german
+apt-get -y install texlive-latex-base
+apt-get -y install texlive-latex-recommended
