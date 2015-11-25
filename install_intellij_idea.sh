03b90d32e5671b1bff6cab1307c614786c1af005
Stefan Birkner <mail@stefan-birkner.de>
2015-11-25 23:01:12 +0100

    Start IntelliJ Idea with Java 8.
    Welcome to the new world.

--- install_intellij_idea.sh
+++ install_intellij_idea.sh
@@ -8,7 +8,7 @@ git clone git://github.com/trygvis/intellij-idea-dpkg.git
 cd intellij-idea-dpkg
 ./build-package -f IU -p debian -u
 dpkg -i repository/debian/intellij-idea-iu-*.deb
-echo "JDK_HOME=/usr/lib/jvm/java-7-oracle" > /etc/default/idea
+echo "JDK_HOME=/usr/lib/jvm/java-8-oracle" > /etc/default/idea
 echo "M2_HOME=/usr/share/maven" >> /etc/default/idea
 cd ..
 rm -r intellij-idea-dpkg
