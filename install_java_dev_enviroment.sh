0943291882561504ffc639b8ae1ad9eefc197b2b
Stefan Birkner <mail@stefan-birkner.de>
2017-11-04 23:40:35 +0100

    Don't install OpenJDK 7 anymore
    It is not available from the repository.

--- install_java_dev_enviroment.sh
+++ install_java_dev_enviroment.sh
@@ -4,6 +4,5 @@
 add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
 apt-get -y install oracle-java8-installer
-apt-get -y install oracle-java7-installer
 apt-get -y install maven
 apt-get -y install gradle
