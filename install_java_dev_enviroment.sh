fb4955c223f1f7ca81c659880aec19f34a5d8985
Stefan Birkner <mail@stefan-birkner.de>
2018-07-15 14:12:59 +0200

    Install Java 10 instead of 9
    Java 9 is end of life. Java 10 is the current version of Java.

--- install_java_dev_enviroment.sh
+++ install_java_dev_enviroment.sh
@@ -2,8 +2,9 @@
 # Install Java development enviroment.
 
 add-apt-repository -y ppa:webupd8team/java
+add-apt-repository ppa:linuxuprising/java
 apt-get -y update
-apt-get -y install oracle-java9-installer
+apt-get -y install oracle-java10-installer
 apt-get -y install oracle-java8-installer
 apt-get -y install maven
 apt-get -y install gradle
