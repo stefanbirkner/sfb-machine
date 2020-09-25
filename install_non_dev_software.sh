0e73d13302f5080bf6a339fd8c6861607c4f2005
Stefan Birkner <mail@stefan-birkner.de>
2020-09-25 21:26:24 +0200

    Replace KeePass with KeePassXC
    KeePassXC has a modern GUI.

--- install_non_dev_software.sh
+++ install_non_dev_software.sh
@@ -7,7 +7,7 @@ apt-get -y update
 apt-get -y install calibre
 apt-get -y install keychain
 apt-get -y install nautilus-dropbox
-apt-get -y install keepass2
+apt-get -y install keepassxc
 apt-get -y install steam
 apt-get -y install texlive-fonts-recommended
 apt-get -y install texlive-lang-german
